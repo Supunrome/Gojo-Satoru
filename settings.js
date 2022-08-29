ce3b9281fd76359b841161bb5def8084a0291921
Supunrome <112425666+Supunrome@users.noreply.github.com>
2022-08-30 00:35:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['+94771790791']
+global.premium = ['+94771790791']
+global.ownernomer = '+94771790791'
+global.ownername = 'isuru pramod'
+global.botname = 'DARK FIGHTER'
+global.footer = '©Shadow'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'Shadow boy'
+global.author = 'isuru pramod'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
